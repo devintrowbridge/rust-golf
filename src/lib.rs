5a828618a61aaaee42fdfec05d7750586fe7b411
Devin Trowbridge <devin.trow@gmail.com>
2022-09-05 09:51:16 -0500

    add running_sum.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 mod lowest_common_ancestor;
 mod word_ladder;
 mod two_sum;
+mod running_sum;
---
+++ src/running_sum.rs
@@ -0,0 +1,25 @@
+struct Solution;
+impl Solution {
+    pub fn running_sum(mut nums: Vec<i32>) -> Vec<i32> {
+        if nums.is_empty() { return nums }
+
+        for i in 1..nums.len() {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+
+        nums
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let nums = vec![2,7,11,15];
+        let target = vec![2, 9, 20, 35];
+        assert_eq!(Solution::running_sum(nums), target)
+    }
+}
