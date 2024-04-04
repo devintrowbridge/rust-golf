5142e0a7cb03bd40edfe2a221c11b7da99ca629e
Devin Trowbridge <dtrowbridge@parrylabs.com>
2024-04-04 15:29:39 -0500

    add problem statements throughout

--- src/move_zeroes.rs
+++ src/move_zeroes.rs
@@ -1,3 +1,10 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+// Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
 pub fn move_zeroes(nums: &mut Vec<i32>) {
     let mut k = 0; // idx of non-zero array
     let mut num_zeros = 0;
--- src/single_num.rs
+++ src/single_num.rs
@@ -1,3 +1,14 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// Example 1:
+// Input: nums = [2,2,1]
+// Output: 1
+
+// Example 2:
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+
 pub fn single_number(nums: Vec<i32>) -> i32 {
     let mut ans = 0x0;
     for n in nums {
